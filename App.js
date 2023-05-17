72cf4b3e49b8b3b06c8e682e769375a9c79e73ac
Lika <14dubinenkoanzhelika@gmail.com>
2023-05-17 19:26:21 +0300

    create register screen

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import photoBg from "./assets/images/photoBG.jpeg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={photoBg}
+        resizeMode="cover"
+        style={styles.backgroundPhoto}>
+        <View style={styles.firstContainer}></View>
+        <View style={styles.innerContainer}>
+          <View style={styles.photoUser}>
+            <View style={styles.addPhoto}>
+              <Text style={styles.plus}>+</Text>
+            </View>
+          </View>
+          <Text style={styles.title}>Реєстрація</Text>
+          <TextInput style={styles.input} placeholder="Логін"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Адреса електронної пошти"></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            secureTextEntry={true}></TextInput>
+          <TouchableOpacity style={styles.registerBtn}>
+            <Text style={styles.registerBtnTitle}>Зареєструватися</Text>
+          </TouchableOpacity>
+          <Text style={styles.anchor}>Вже є аккаунт? Увійти</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +45,85 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  backgroundPhoto: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  firstContainer: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 2,
+    // height: 549,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  photoUser: {
+    backgroundColor: "#F6F6F6",
+    height: 120,
+    width: 120,
+    borderRadius: 16,
+    top: -60,
+    left: 128,
+    elevation: 10,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  addPhoto: {
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    height: 25,
+    width: 25,
+    top: -14,
+    left: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  plus: {
+    color: "#FF6C00",
+    fontSize: 20,
+    bottom: 3,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 500,
+    // height: 35,
+    // marginTop: 32,
+    marginBottom: 16,
+    marginHorizontal: 110,
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#F6F6F6",
+    marginTop: 16,
+    marginHorizontal: 16,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    paddingLeft: 16,
+  },
+  registerBtn: {
+    backgroundColor: "#FF6C00",
+    marginHorizontal: 16,
+    borderRadius: 100,
+    marginTop: 43,
+    paddingVertical: 16,
+    alignItems: "center",
+  },
+  registerBtnTitle: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  anchor: {
+    color: "#1B4371",
+    fontSize: 16,
+    marginTop: 16,
+    marginHorizontal: 94,
   },
 });
